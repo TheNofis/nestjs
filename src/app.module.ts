3668855df0a1cec7b0f0d96e31f1e63364fade2e
TheNofis <matbey122@gmail.com>
2025-03-06 16:06:44 +0300

    ref: remove trash

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 import { ChatModule } from './chat/chat.module';
-import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ChatModule, ChatsModule],
+  imports: [UserModule, AuthModule, ChatModule],
   controllers: [],
 })
 export class AppModule {}
