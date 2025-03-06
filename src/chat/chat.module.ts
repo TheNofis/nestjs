049781d00bcbc256e4d63528ddf41938211c1012
TheNofis <matbey122@gmail.com>
2025-03-06 14:27:29 +0300

    add: redis service

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,9 +3,10 @@ import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { AuthModule } from 'src/auth/auth.module';
 import { PrismaService } from 'src/databases/prisma/prisma.service';
+import { RedisService } from 'src/databases/redis/redis.service';
 
 @Module({
   imports: [AuthModule],
-  providers: [PrismaService, ChatGateway, ChatService],
+  providers: [PrismaService, RedisService, ChatGateway, ChatService],
 })
 export class ChatModule {}
